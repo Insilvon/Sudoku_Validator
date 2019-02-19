9c168f5bc4e1d420e93630a55aaa1ee68477cda2
BuildTools <unconfigured@null.spigotmc.org>
2019-02-19 15:05:46 -0500

    Infrastructure for Sudoku Puzzle Data Structure complete

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,48 +1,142 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Puzzle {
 
-    public String[][] grid = new String[3][3];
-    public Puzzle(File theFile) {
+    private String[][] grid = new String[9][9];
 
+    public Puzzle(File theFile) {
         try {
             Scanner reader = new Scanner(theFile);
             int row = 0;
-            while(reader.hasNextLine()){
+            while(reader.hasNextLine()) {
                 //get the line in the file
-                String line = reader.nextLine();
+                StringBuilder line = new StringBuilder(reader.nextLine());
                 //remove any decimals
-                line.replace('.','');
-                //split the line into modules by spaces
-                String[] characters = line.split(" ");
-                //check for line length and validate here
-                if (characters.length!=9) {
-                    System.out.println("Invalid length!");
-                } else {
-                    //loop through the split characters and add them to the grid
-                    for (int i = 0; i< characters.length; i++){
-                        grid[row][i] = characters[i];
-                    }
-                }
+                while (line.indexOf(".")!=-1) line.deleteCharAt(line.indexOf("."));
+                //convert to array
+                String[] characters = line.toString().split(" ");
+                //slap it into the 2D array
+                if (characters.length!=9) System.out.println("Invalid length!");
+                else System.arraycopy(characters, 0, grid[row], 0, characters.length);
                 row++;
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
     }
 
     /**
      * Prints out the contents of the matrix
      */
     public void printMatrix(){
-        for(int i = 0; i<3; i++){
-            for (int j = 0; j<3; j++){
+        for(int i = 0; i<9; i++){
+            for (int j = 0; j<9; j++){
                 System.out.print(this.grid[i][j]+" ");
             }
             System.out.println();
         }
+    }
+
+    /**
+     * Fetches a row from the grid
+     * @param index - the row number specified
+     * @return new string[] of required numbers
+     */
+    public String[] getRow(int index){
+        if (index<0 || index>8) {
+            System.out.println("ERROR: INDEX OUT OF BOUNDS");
+            return null;
+        }
+        return this.grid[index];
+    }
+
+    /**
+     * Fetches a column from the grid
+     * @param index - the column number to be retrieved
+     * @return new string[] of required items
+     */
+    public String[] getCol(int index){
+        if (index<0 || index>8) {
+            System.out.println("ERROR: INDEX OUT OF BOUNDS");
+            return null;
+        }
+        ArrayList<String> temp = new ArrayList<String>();
+        for (int i = 0; i<9;i++) temp.add(this.grid[i][index]);
+        String[] temp2 = new String[temp.size()];
+        for (int i = 0; i<temp.size(); i++) temp2[i] = temp.get(i);
+        return temp2;
+    }
+
+    /**
+     * Method which will fetch a block from the grid
+     * @param non - the (not a quadrant) block to fetch, labeled t->b, l->r, 1-9
+     * @return String[] of all values in the block
+     */
+    public String[] getBlock(int non){
+        ArrayList<String> temp = new ArrayList<String>();
+        int sRowIndex, eRowIndex, sColIndex, eColIndex;
+        switch(non){
+            case 1:
+                eColIndex = eRowIndex = 3;
+                sColIndex = sRowIndex = 0;
+                break;
+            case 2:
+                sRowIndex = 0;
+                eRowIndex = 3;
+                sColIndex = 3;
+                eColIndex = 6;
+                break;
+            case 3:
+                sRowIndex = 0;
+                eRowIndex = 3;
+                sColIndex = 6;
+                eColIndex = 9;
+                break;
+            case 4:
+                sRowIndex = 3;
+                eRowIndex = 6;
+                sColIndex = 0;
+                eColIndex = 3;
+                break;
+            case 5:
+                sRowIndex = sColIndex = 3;
+                eRowIndex = eColIndex = 6;
+            case 6:
+                sRowIndex = 3;
+                eRowIndex = 6;
+                sColIndex = 6;
+                eColIndex = 9;
+                break;
+            case 7:
+                sRowIndex = 6;
+                eRowIndex = 9;
+                sColIndex = 0;
+                eColIndex = 3;
+                break;
+            case 8:
+                sRowIndex = 6;
+                eRowIndex = 9;
+                sColIndex = 3;
+                eColIndex = 6;
+                break;
+            case 9:
+                sRowIndex = sColIndex = 6;
+                eRowIndex = eColIndex = 9;
+                break;
+            default:
+                System.out.println("ERROR: Requested grid out of bounds");
+                return null;
+        }
+        for(int i = sRowIndex; i<eRowIndex; i++){
+            for (int j = sColIndex; j<eColIndex; j++){
+                temp.add(this.grid[i][j]);
+            }
+        }
+        String[] temp2 = new String[temp.size()];
+        for (int i = 0; i<temp.size(); i++) temp2[i] = temp.get(i);
+        return temp2;
     }
 }
---
+++ src/main.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        File file;
+        while (true) {
+            System.out.println("File name to look for?");
+            String input = in.nextLine();
+            String file_name = "./"+input;
+            file = new File(file_name);
+            if (file.exists()) break;
+            else System.out.println("File does not exist. Try again?");
+        }
+        Puzzle myPuzzle = new Puzzle(file);
+        myPuzzle.printMatrix();
+        System.out.println("================================");
+        String[] temp = myPuzzle.getCol(0);
+        for (int i = 0; i<temp.length; i++){
+            System.out.print(temp[i]);
+        }
+        System.out.println();
+        System.out.println("================================");
+        temp = myPuzzle.getRow(0);
+        for (int i = 0; i<temp.length; i++){
+            System.out.print(temp[i]);
+        }
+        System.out.println();
+        System.out.println("================================");
+        temp = myPuzzle.getBlock(1);
+        int check = 0;
+        for (int i = 0; i<temp.length; i++){
+            if (check == 3){
+                check = 0;
+                System.out.println();
+            }
+            System.out.print(temp[i]);
+            check++;
+        }
+        System.out.println();
+    }
+}
